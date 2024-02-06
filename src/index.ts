fdec277328f249cda8ae47f49515e5365ff19027
arpesh28 <arpeshgadekar@gmail.com>
2024-02-06 09:20:31 +0530

    update: add timestamps in user models

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 import { rootRouter } from "./routes/index.route";
 
-// import connectDB from "./config/db.config";
-// connectDB();
+import connectDB from "./config/db.config";
+connectDB();
 
 const app: Express = express();
 
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
-app.get("/api", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,24 +1,27 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      kMaxLength: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    kMaxLength: 50,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 export { User };
