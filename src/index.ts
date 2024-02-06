673f316a52ccb640e1d1a839502e5b2df9fabbf2
arpesh28 <arpeshgadekar@gmail.com>
2024-02-06 12:31:32 +0530

    update: add root get route in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-dotenv.config();
-import { rootRouter } from "./routes/index.route";
+// import express, { Express, Request, Response } from "express";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import { rootRouter } from "./routes/index.route";
 
-import connectDB from "./config/db.config";
-connectDB();
+// import connectDB from "./config/db.config";
+// connectDB();
+
+// const app: Express = express();
+
+// app.use(express.json());
+
+// app.use("/api/v1", rootRouter);
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello World!");
+// });
+
+// const PORT = process.env.PORT || 8001;
+
+// app.listen(PORT, () => {
+//   console.log("Running on port: ", PORT);
+// });
+
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 
 app.use(express.json());
 
-app.use("/api/v1", rootRouter);
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 8001;
+const PORT = 8001;
 
 app.listen(PORT, () => {
   console.log("Running on port: ", PORT);
