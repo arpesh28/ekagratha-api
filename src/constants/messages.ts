3a49f228f78c540bfe224cdb4e94e0e3e5854567
arpesh28 <arpeshgadekar@gmail.com>
2024-02-10 12:36:46 +0530

    update: google authentication complete with saving user to db

---
+++ src/constants/messages.ts
@@ -0,0 +1,12 @@
+export const errorMessages = {
+  // Common Errors
+  SOMETHING_WRONG: "Something went wrong",
+  //    Auth Errors
+  EMAIL_REGISTERED_WITH_OTHER_PROVIDER:
+    "Email address is registered with another provider",
+  UNKNOWN_SUBSCRIBER_ID: "Unknown Subscriber id",
+  EMAIL_NOT_FOUND: "Email not found!",
+  INVALID_CREDENTIALS: "Email not found!",
+  GOOGLE_CODE_MISSING: "Google Authorization Code is required!",
+  GOOGLE_AUTH_FAILED: "Failed to authenticate with Google",
+};
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,58 +3,89 @@ import { User } from "../models/User.model";
 import { bcryptPassword, comparePasswords } from "../config/helpers";
 import jwt from "jsonwebtoken";
 import axios from "axios";
+import { Providers } from "../typings/enum";
+import { errorMessages } from "../constants/messages";
+import { ProviderUserProfile } from "../typings/types";
 
 // Local Register
 const registerController = async (req: Request, res: Response) => {
-  const existingUser = await User.findOne({ email: req.body.email });
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
 
-  if (existingUser) return res.json({ message: "Email already exists!" });
+    if (existingUser)
+      return res.status(400).json({ message: "Email already exists!" });
 
-  const hashPassword = bcryptPassword(req.body.password);
-  const user = await User.create({
-    name: req.body.name,
-    password: hashPassword,
-    email: req.body.email,
-  });
+    const hashPassword = bcryptPassword(req.body.password);
+    const user = await User.create({
+      name: req.body.name,
+      password: hashPassword,
+      email: req.body.email,
+    });
 
-  if (!user) return res.json({ message: "Something went wrong" });
+    if (!user)
+      return res.status(500).json({ message: errorMessages.SOMETHING_WRONG });
 
-  const token = jwt.sign(
-    { name: user.name, email: user.email },
-    process.env.JWT_SECRET!
-  );
-  res.json({
-    data: {
-      user: { email: user.email, _id: user._id, name: user.name },
-      token,
-    },
-  });
+    const token = jwt.sign(
+      { name: user.name, email: user.email },
+      process.env.JWT_SECRET!
+    );
+    res.json({
+      data: {
+        user: {
+          email: user.email,
+          _id: user._id,
+          name: user.name,
+          provider: user.provider,
+        },
+        token,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: errorMessages.SOMETHING_WRONG });
+  }
 };
 
 // Local Login
 const loginController = async (req: Request, res: Response) => {
-  const user = await User.findOne({ email: req.body.email });
+  try {
+    const user = await User.findOne({ email: req.body.email });
 
-  if (!user) return res.status(401).json({ message: "Email not found!" });
+    if (!user)
+      return res.status(401).json({ message: errorMessages.EMAIL_NOT_FOUND });
 
-  const validPass: boolean = await comparePasswords(
-    req.body.password,
-    user.password
-  );
-  if (!validPass)
-    return res.status(401).json({ message: "Email/Password is incorrect" });
+    if (user.provider !== Providers.Email || !user.password)
+      return res
+        .status(400)
+        .json({ message: errorMessages.EMAIL_REGISTERED_WITH_OTHER_PROVIDER });
 
-  const token = jwt.sign(
-    { email: user.email, name: user.name },
-    process.env.JWT_SECRET!
-  );
+    const validPass: boolean = await comparePasswords(
+      req.body.password,
+      user.password
+    );
+    if (!validPass)
+      return res
+        .status(401)
+        .json({ message: errorMessages.INVALID_CREDENTIALS });
 
-  res.json({
-    data: {
-      user: { email: user.email, name: user.name, _id: user._id },
-      token,
-    },
-  });
+    const token = jwt.sign(
+      { email: user.email, name: user.name },
+      process.env.JWT_SECRET!
+    );
+
+    res.json({
+      data: {
+        user: {
+          email: user.email,
+          name: user.name,
+          _id: user._id,
+          provider: user.provider,
+        },
+        token,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: errorMessages.SOMETHING_WRONG });
+  }
 };
 
 // Get Google Auth URL
@@ -71,7 +102,7 @@ const googleAuthCallbackController = async (req: Request, res: Response) => {
 
   // Validate google authentication code
   if (!code)
-    res.status(400).json({ message: "Google Authorization Code is required!" });
+    res.status(400).json({ message: errorMessages.GOOGLE_CODE_MISSING });
 
   try {
     // Exchange code for token from google
@@ -96,14 +127,73 @@ const googleAuthCallbackController = async (req: Request, res: Response) => {
         },
       }
     );
-    const userProfile = profileResponse.data; // User profile that will be stored in the DB
+    const userProfile: ProviderUserProfile = profileResponse.data; // User profile that will be stored in the DB
 
-    res.json({ tokenResponse: tokenResponse?.data, userProfile });
+    const existingUser = await User.findOne({ email: userProfile?.email });
+
+    if (existingUser) {
+      if (existingUser.provider !== Providers.Google)
+        return res.status(400).json({
+          message: errorMessages.EMAIL_REGISTERED_WITH_OTHER_PROVIDER,
+        });
+
+      if (existingUser.providerUserId !== userProfile.sub) {
+        return res.status(401).json({
+          message: errorMessages.UNKNOWN_SUBSCRIBER_ID,
+        });
+      }
+      const token = jwt.sign(
+        {
+          email: existingUser.email,
+          name: existingUser.name,
+          _id: existingUser._id,
+        },
+        process.env.JWT_SECRET!
+      );
+      return res.json({
+        data: {
+          user: {
+            email: existingUser.email,
+            name: existingUser.name,
+            _id: existingUser._id,
+            provider: existingUser.provider,
+          },
+          token,
+        },
+      });
+    }
+
+    const newUser = await User.create({
+      email: userProfile.email,
+      name: userProfile.name,
+      providerUserId: userProfile.sub,
+      provider: Providers.Google,
+    });
+
+    if (!newUser)
+      return res.status(500).json({
+        message: errorMessages.SOMETHING_WRONG,
+      });
+
+    const token = jwt.sign(
+      {
+        email: newUser.email,
+        name: newUser.name,
+        _id: newUser._id,
+        provider: newUser.provider,
+      },
+      process.env.JWT_SECRET!
+    );
+
+    res.json({
+      data: {
+        user: newUser,
+        token,
+      },
+    });
   } catch (error: any) {
     console.error("Error exchanging code for token:", error?.response?.data);
-    res
-      .status(500)
-      .json({ message: "Failed to authenticate with Google", error });
+    res.status(500).json({ message: errorMessages.GOOGLE_AUTH_FAILED });
   }
 };
 
