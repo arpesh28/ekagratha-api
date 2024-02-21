c1b138f5699b9be0830a37125a157bf2bb1d28d1
Omkar Vaigankar <42296321+omkarvaigankar8@users.noreply.github.com>
2024-02-21 19:50:08 +0530

    minor fixes

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { Task } from "../models/Task.model";
 import { errorMessages, successMessages } from "../constants/messages";
+import { User } from "../models/User.model";
 
 // Create Personal Task
 const createPersonalTaskController = async (req: Request, res: Response) => {
@@ -11,24 +12,33 @@ const createPersonalTaskController = async (req: Request, res: Response) => {
             priority: req.body.priority,
             userId: req.body.user._id
         });
-        return res.json(200).json({
+
+        // Update user document to add task ID to personalTasks array
+        await User.findByIdAndUpdate(req.body.user._id, {
+            $push: { personalTasks: task._id }
+        });
+
+        // Send a single response after creating the task and updating the user
+        return res.status(200).json({
             message: successMessages.TASK_ADDED,
             data: {
                 task: { title: task.title, _id: task._id, description: task.description, priority: task.priority },
             },
-        })
+        });
     } catch (error) {
+        // Handle any errors that occur during task creation or user update
         return res.status(500).json({
             message: errorMessages.SOMETHING_WRONG,
         });
     }
 };
 
+
 // Fetch Personal Task
 const getPersonalTaskController = async (req: Request, res: Response) => {
     try {
         const tasks = await Task.find({ userId: req?.body?.user?._id });
-        if (!tasks) return res.status(404).json({ message: errorMessages.TASK_NOT_FOUND });
+        if (tasks.length === 0) return res.status(404).json({ message: errorMessages.TASK_NOT_FOUND });
         return res.json(200).json({
             message: successMessages.TASK_FETCHED,
             data: tasks
@@ -61,7 +71,6 @@ const updatePersonalTaskController = async (req: Request, res: Response) => {
             }
         });
     } catch (error) {
-        console.log("error", error)
         return res.status(500).json({
             message: errorMessages.TASK_ID_INCORRECT
         });
@@ -75,6 +84,11 @@ const deletePersonalTaskController = async (req: Request, res: Response) => {
     try {
         const deletedTask = await Task.findOneAndDelete({ _id: taskId });
         if (deletedTask) {
+            // Remove deleted task ID from the personalTasks array of the corresponding user
+            await User.findByIdAndUpdate(deletedTask.userId, {
+                $pull: { personalTasks: taskId }
+            });
+
             return res.status(200).json({ message: successMessages.TASK_DELETED });
         } else {
             return res.status(404).json({ message: errorMessages.TASK_ALREADY_DELETED });
@@ -85,6 +99,7 @@ const deletePersonalTaskController = async (req: Request, res: Response) => {
         });
     }
 }
+
 export {
     createPersonalTaskController,
     getPersonalTaskController,
